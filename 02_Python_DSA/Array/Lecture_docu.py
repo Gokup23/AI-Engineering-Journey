a71665ca20cbaa75abf975b6df8a32e7c51a79a3
Gokup23 <976575334a@gmail.com>
2026-02-10 06:46:24 +0530

    WIP: save local changes

--- 02_Python_DSA/Array/Lecture_docu.py
+++ 02_Python_DSA/Array/Lecture_docu.py
@@ -1,5 +1,7 @@
+#################################### Array Module ####################################
 # import array as arr     -     this way needs to be prefixed with arr. everytime
 from array import *  # - this way can be used directly
+#Aliasing - alternative names for existing Python modules or objects
 
 
 val = array('i',[1,2,3,4,5,6])
@@ -26,3 +28,31 @@ copyArray = array(val.typecode , (x*3 for x in val)) #copying array with modifie
 #Searching:
 #arr.index(value): Returns the index of the first occurrence of the value. Raises an error if not found.
 # Slicing: arr[start:end:step]
+
+#Taking input from user to make array
+arr = array('i',[])
+n = int(input('Enter  a  number (size of array) '))
+for i in range(0,n):
+    arr.append(int(input('Enter next value ')))
+for x in arr:
+    print(x,end ="")
+
+################################### Numpy Library ####################################
+from numpy import *
+#float - typecasting possible
+val1 = array([1,2,3,4.5] )
+for x in val1:
+    print(x,end=" ")
+
+#Multideimentional arrays
+
+zero = array(10)
+
+one = array([1,2,3,4,5,6])
+
+two = array([[1,2,3],[4,5,6],[7,8,9]])
+
+three = array([ [ [1,2], [3,4]  ] , [ [5,6] , [7,8] ] ])
+#Dimensions must be same to get output without error
+
+
