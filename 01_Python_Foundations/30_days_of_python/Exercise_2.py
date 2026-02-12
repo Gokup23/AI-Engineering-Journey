05fa26adbf6aae6ffd60ea872e1e688b31481b3f
Gokup23 <976575334a@gmail.com>
2026-02-12 08:16:32 +0530

    Local changes

--- 01_Python_Foundations/30_days_of_python/Exercise_2.py
+++ 01_Python_Foundations/30_days_of_python/Exercise_2.py
@@ -163,3 +163,47 @@ Seconds given: 705456000
 4 1 4 16 64
 5 1 5 25 125
 >>> 
+
+"""
+Questions.
+1.Declare your age as integer variable
+2.Declare your height as a float variable
+3.Declare a variable that store a complex number
+4.Write a script that prompts the user to enter base and height of the triangle and calculate an area of this triangle (area = 0.5 x b x h).
+    Enter base: 20
+    Enter height: 10
+    The area of the triangle is 100
+5.Write a script that prompts the user to enter side a, side b, and side c of the triangle. Calculate the perimeter of the triangle (perimeter = a + b + c).
+Enter side a: 5
+Enter side b: 4
+Enter side c: 3
+6.The perimeter of the triangle is 12
+7.Get length and width of a rectangle using prompt. Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+8.Get radius of a circle using prompt. Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+9.Calculate the slope, x-intercept and y-intercept of y = 2x -2
+10.Slope is (m = y2-y1/x2-x1). Find the slope and Euclidean distance between point (2, 2) and point (6,10)
+11.Compare the slopes in tasks 8 and 9.
+12.Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is going to be 0.
+13.Find the length of 'python' and 'dragon' and make a falsy comparison statement.
+14.Use and operator to check if 'on' is found in both 'python' and 'dragon'
+15.I hope this course is not full of jargon. Use in operator to check if jargon is in the sentence.
+16.There is no 'on' in both dragon and python
+17.Find the length of the text python and convert the value to float and convert it to string
+18.Even numbers are divisible by 2 and the remainder is zero. How do you check if a number is even or not using python?
+19.Check if the floor division of 7 by 3 is equal to the int converted value of 2.7.
+20.Check if type of '10' is equal to type of 10
+21.Check if int('9.8') is equal to 10
+22.Write a script that prompts the user to enter hours and rate per hour. Calculate pay of the person?
+Enter hours: 40
+Enter rate per hour: 28
+Your weekly earning is 1120
+23.Write a script that prompts the user to enter number of years. Calculate the number of seconds a person can live. Assume a person can live hundred years
+Enter number of years you have lived: 100
+You have lived for 3153600000 seconds.
+24.Write a Python script that displays the following table
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125
+"""
