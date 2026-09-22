8cd4c9598611677f418af691f649b9395ec0baa9
Gokup23 <976575334a@gmail.com>
2026-09-22 20:20:41 +0530

    changes

---
+++ 01_Python_Foundations/30_days_of_python/Exercise_9.py
@@ -0,0 +1,18 @@
+#Loops
+
+#Exercise_level1
+'''
+for item in range(10,-1,-1):
+    print(item)
+n = 10
+while n>=0:
+    print(n)
+    n-=1 
+'''
+i = 0
+j = 0
+for i in range(7):
+    for j in range(i):
+        print("#"*j) 
+i += 1 
+
